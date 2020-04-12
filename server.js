2596fedb9f681f5702a8e6b2752566753ca018ff
Zanma <zainurimahmud32@gmail.com>
2020-04-13 06:49:30 +0700

    add body-parser

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('helloo');
-    next();
-})
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.send('test test')
+    res.send('getting root')
+});
+
+app.get('/profile', (req, res) => {
+    res.send('getting profile')
+});
+
+app.post('/profile', (req, res) => {
+    console.log(req.body);
+    res.send('success');
 });
 
 app.listen(3000)
